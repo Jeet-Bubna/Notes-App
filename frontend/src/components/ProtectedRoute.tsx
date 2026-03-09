1a833fdda0df63dc0f9b77eda2f25771f71b0b78
Jeet Bubna <bubnajeet@gmail.com>
2026-03-09 11:24:51 +0530

    added protected routes in frontend

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,75 @@
+import { jwtDecode } from "jwt-decode";
+import { ACCESS_TOKEN, REFRESH_TOKEN} from "../constants";
+import { useEffect, useState, type ReactNode } from "react";
+import api from "../api";
+import { Navigate } from "react-router-dom";
+
+interface ProtectedRouteProps {
+    children: ReactNode
+}
+
+function ProtectedRoute ({children}:ProtectedRouteProps) {
+    const [isAuthorized, setAuthorize] = useState<null | boolean>(null)
+    
+    useEffect(() => {
+        auth().catch(() => setAuthorize(false));
+    }, [])
+
+    const refreshToken = async () =>  {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        try {
+            const resp = await api.post("/api/token/refresh/", {refresh: refreshToken,})
+
+            if (resp.status === 200){
+                localStorage.setItem(ACCESS_TOKEN, resp.data.access)
+                setAuthorize(true)
+            } else {
+                setAuthorize(false)
+            }
+
+        } catch (error){
+            console.log(error)
+            setAuthorize(false)
+        }
+
+    }
+
+    const auth = async () => {
+
+        const access_token = localStorage.getItem(ACCESS_TOKEN)
+
+        if ( !access_token) {
+            setAuthorize(false)
+            return
+        }
+
+        const decoded = jwtDecode(access_token)
+        
+        
+        const expiration = decoded.exp
+
+        if (!expiration ){
+            setAuthorize(false)
+            console.log("error occured in jwt")
+            return
+        }
+
+        const now = Date.now() / 1000 // to get it in seconds
+
+        if (expiration < now) {
+            await refreshToken()
+
+        } else {
+            setAuthorize(true)
+        }
+
+        if (isAuthorized === null){
+            return <div>Loading...</div>
+        
+        }
+        
+        return isAuthorized ? children : <Navigate to="/login"/>
+    }
+}
+
+export default ProtectedRoute
